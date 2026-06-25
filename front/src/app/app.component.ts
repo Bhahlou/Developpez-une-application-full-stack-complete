711500e26957d4c50b3955e8dbfb32f5712e10d9
Bhahlou <bhahlou@gmail.com>
2026-06-25 19:57:23 +0200

    chore(front): upgrade @angular/material and @angular/cdk to v22

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.Eager,
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Eager,
 })
 export class AppComponent {
   title = 'front';
