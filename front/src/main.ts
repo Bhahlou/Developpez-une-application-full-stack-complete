474ee0f30d43ca0348ebe75cb2941cecbb815158
Bhahlou <bhahlou@gmail.com>
2026-06-25 21:29:15 +0200

    refactor(front): convert app to standalone components and bootstrapApplication

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,12 +1,17 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, MatButtonModule),
+  ],
+}).catch((err) => console.error(err));
